2849e2ecd0921844e4734bb87b1e2f85ae4fe04b
coreytegeler <coreytegeler@gmail.com>
2015-05-01 17:06:49 -0400

    removed shitty migration file

--- db/migrate/20150205191652_rename_rows_in_join_table.rb
+++
@@ -1,8 +0,0 @@
-class RenameRowsInJoinTable < ActiveRecord::Migration
-  def up
-  	rename_column("admins_groups", "user_id", "admin_id")
-  end
-  def down
-	rename_column("admins_groups", "admin_id", "user_id")
-  end
-end
