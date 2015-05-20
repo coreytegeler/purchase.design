c9e5e5d2a438cd04f7061895b8004aa40bf7ed0c
coreytegeler <coreytegeler@gmail.com>
2015-05-20 09:50:28 -0400

    remove name creation from palette

--- app/models/palette.rb
+++ app/models/palette.rb
@@ -2,7 +2,6 @@ class Palette < ActiveRecord::Base
 
 	scope :first_to_last, lambda {order("palettes.position ASC")}
 
-	before_validation :create_name
 	before_validation :add_hash_symbol
 
 	validates :primary_color, :hex_color => true
@@ -11,12 +10,6 @@ class Palette < ActiveRecord::Base
 	acts_as_list :order => :position
 
 	private
-
-		def create_name
-			if name.blank?
-				self.name = "Palette No." + " #{position}"
-			end
-		end
 
 		def add_hash_symbol
 			if primary_color[0] != "#"
