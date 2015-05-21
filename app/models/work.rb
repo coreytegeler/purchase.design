d3592e385d40f8c20eabf67f15229c16b99a5614
coreytegeler <coreytegeler@gmail.com>
2015-05-21 12:45:48 -0400

    lang set and removed active_link_tos

--- app/models/work.rb
+++ app/models/work.rb
@@ -11,20 +11,16 @@ class Work < ActiveRecord::Base
 		:medium => ['450x450'],
 		:large =>  ['1200x1200>']
 	},
-	# :convert_options => {
-	# 	:small =>  ['-set colorspace sRGB -strip'],
-	# 	:medium => ['-set colorspace sRGB -strip'],
-	# 	:large =>  ['-set colorspace sRGB -strip -sharpen 0x0.5']
-	# },
 	:default_url => "image.svg"
 		
   	validates_attachment_content_type :image, 
-  content_type: /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)/
+  		content_type: /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)/
 
-    has_attached_file :video, :styles => {
-            :medium => { :geometry => "600x450", :format => 'mp4'},
-            :thumb => {:geometry => "300x300#", :format => 'jpg', :time => 10}
-        }, :processors => [:ffmpeg]
+    has_attached_file :video,
+    :styles => {
+        :medium => { :geometry => "600x450", :format => 'mp4'},
+        :thumb => {:geometry => "300x300#", :format => 'jpg', :time => 10}
+    }, :processors => [:ffmpeg]
 
     validates_attachment_content_type :video, content_type: /\Avideo\/.*\Z/ 
 
