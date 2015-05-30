d310380592b1d5d5defcaaf4e07794a0352fd335
coreytegeler <coreytegeler@gmail.com>
2015-05-30 17:53:42 -0400

    center stack

--- app/assets/javascripts/works.js
+++ app/assets/javascripts/works.js
@@ -1,13 +1,14 @@
 var initWorks = function() {
 	if($('body').is('.public')) {
-		//GET DYNAMIC NUMBERS
-		var maxWidth = 602;
-		var maxHeight = 456;
-		var stackHeight;
-		var stackWidth;
-		var gap = 5;
-
 		$('#stack').ready(function() {
+			var maxWidth = 525;
+			var maxHeight = 450;
+			var stackHeight;
+			var stackWidth;
+			var gap = 5;
+			var stackSize = $('#stack .work').length;
+			var gaps = stackSize * gap - gap;
+			$('.public #stack').css({height: maxHeight + gaps + gap*2, width: maxWidth + gaps + gap*2});
 			$('#stack .work').each(function(i, w) {
 				$(w).addClass('loaded').css({y: -gap*i, x: gap*i});
 				if(!$(w).children('.designer').length) {
@@ -16,21 +17,12 @@ var initWorks = function() {
 			});
 		});
 
-		$('#stack').ready(function() {
-			var gaps = ($('.media_wrapper').length * gap - gap);
-			$('.public #stack').css({height: maxHeight + gaps, width: maxWidth + gaps});
-		});
-
 		$('#stack .work').click(function(e) {
 			var oldTop = $('.work:last-child');
 			var oldTopPosition = $(oldTop).data('position');
 			$(oldTop).insertBefore('.work:first-child');
 			var oldTooltip = $('.designer[data-position='+oldTopPosition+']');
 			$(oldTooltip).css({'display' : 'none'});
-
-			// var newTop = $('.work:last-child');
-			// var newTopPosition = $(newTop).data('position');
-			// var newTooltip = $('.designer#'+newTopPosition);
 
 			$('.work').each(function(i, w) {
 				$(w).css({y: -gap*i, x: gap*i});
@@ -42,8 +34,6 @@ var initWorks = function() {
 					}
 				}
 			});
-			
-			
 		});
 	} else {
 		$('select#work_media_type').change(function(e) {
