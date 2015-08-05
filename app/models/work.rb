626359d397b8c743f77a8dca96004351b7349130
coreytegeler <coreytegeler@gmail.com>
2015-08-05 19:52:33 -0400

    flv

--- app/models/work.rb
+++ app/models/work.rb
@@ -20,10 +20,10 @@ class Work < ActiveRecord::Base
     # }, :processors => [:ffmpeg]
 
     has_attached_file :video,
-     :styles => {
-	    :medium => { :geometry => "640x480", :format => 'mp4' }
+    :styles => {
+	    :medium => { :geometry => "640x480", :format => 'flv' }
 	    # :thumb => { :geometry => "100x100#", :format => 'jpg', :time => 10 }
-  	}, :processors => [:transcoder]
+  	}
 
     validates_attachment_content_type :video, :content_type => ['video/mp4']
 
