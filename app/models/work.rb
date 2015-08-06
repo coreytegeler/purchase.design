92bcc35b9b2706bbd467e75478faae7c8d0106a0
coreytegeler <coreytegeler@gmail.com>
2015-08-05 22:43:45 -0400

    transcode to mp4

--- app/models/work.rb
+++ app/models/work.rb
@@ -14,7 +14,7 @@ class Work < ActiveRecord::Base
   		content_type: /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)/
 
     has_attached_file :video, :styles => {
-    	:medium => { :geometry => "640x480", :format => 'mp4' },
+    	# :medium => { :geometry => "640x480", :format => 'mp4' },
     	:thumb => { :geometry => "100x100#", :format => 'jpg', :time => 10 }
   	}, :processors => [:transcoder]
 
